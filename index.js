cef0f655344fb09ae125e8b879b5e26ac52e0a86
fahmipraga <itmamul2004@gmail.com>
2023-08-05 21:27:53 +0700

    fix: express urlencoded & express json

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ require("dotenv").config({
 
 const express = require("express");
 const app = express();
+app.use(express.json({ limit: "10mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 })
+);
 const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
 app.use("/", require("./src/routers"));
 
 app.get("/", (req, res) => {
