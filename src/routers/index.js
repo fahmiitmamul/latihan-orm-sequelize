a93d6a279bb3a0ca34e685eed9b7cce033af4847
fahmipraga <itmamul2004@gmail.com>
2023-08-06 12:38:21 +0700

    feat: order router

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use("/auth", require("./auth.router"));
 router.use("/product", require("./product.router"));
+router.use("/orders", require("./order.router"));
 
 module.exports = router;
